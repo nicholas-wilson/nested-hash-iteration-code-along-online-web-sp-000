b579081b5745cf1524f2d83e28286237a51f72b2
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-21 03:26:41 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,13 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, info|
+    if person == "Freddy Mercury"
+      info.each do |element, value|
+        if element == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
+      end
+    end
+  end
 end
